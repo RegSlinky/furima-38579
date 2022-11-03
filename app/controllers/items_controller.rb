953d902c0f283c83fc9d9606a2251aeb18be8a6e
RegSlinky <thesmile0701@gmail.com>
2022-11-03 17:26:00 +0900

    コントローラー内のリファクタリング2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :new, :create, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, except: [:index, :new, :create, :show]
   
   def index
     @items = Item.order("created_at DESC")
@@ -46,7 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     @user = @item.user
     unless user_signed_in? && current_user.id == @user.id
       redirect_to action: :index
