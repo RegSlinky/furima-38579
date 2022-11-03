63c889eb97fbd0685623d528e4be15fb7833d8f1
RegSlinky <thesmile0701@gmail.com>
2022-11-03 16:42:17 +0900

    ログイン状態での条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :new, :create, :show]
   
   def index
     @items = Item.order("created_at DESC")
@@ -27,14 +28,27 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item =  Item.find(params[:id])
-    item.update(item_params)
+    @item =  Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id), method: :get
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :item, :item_text, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :schedule_delivery_id, :price).merge(user_id:current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    @user = @item.user
+    unless user_signed_in? && current_user.id == @user.id
+      redirect_to action: :index
+    end
   end
 
 end
