2ff6f6a152e2c98657798e81aef25737ba915205
RegSlinky <thesmile0701@gmail.com>
2022-11-04 16:15:44 +0900

    商品名・商品画像・価格・配送料の負担」の4つの情報の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,23 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @order = Order.new(order_params)
   end
 
   def create
+    @order = Order.create.new(order_params)
+    Destination.create.new(destination_params)
+    redirect_to root_path
   end
+
+  private
+
+  def order_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def destination_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(order_id: @order.id)
+  end
+
 end
