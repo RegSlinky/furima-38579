831d05febdadfa502d001bdd66c32f5eb655f3f3
RegSlinky <thesmile0701@gmail.com>
2022-11-10 20:40:03 +0900

    config/deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock '3.17.1'
 set :application, 'furima-38579'
 
 set :repo_url,  'git@github.com:RegSlinky/furima-38579.git'
+set :branch, "main"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -14,11 +15,9 @@ set :ssh_options, auth_methods: ['publickey'],
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
