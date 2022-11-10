311e1900f1d80652976085f91c3f2440a8c9dffe
RegSlinky <thesmile0701@gmail.com>
2022-11-10 20:17:00 +0900

    自動デプロイの実装

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+lock '3.17.1'
+
+set :application, 'furima-38579'
+
+set :repo_url,  'git@github.com:RegSlinky/furima-38579.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/stbk1937.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
